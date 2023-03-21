ee7981b0f81ef24eef5301c81e1228ca927ac30d
iael <jcs72484@udelasalle.edu.mx>
2023-03-21 16:14:29 -0600

    first commit

---
+++ api/users.js
@@ -0,0 +1,8 @@
+const UserController = require('../controller/users');
+const express = require('express');
+const router = express.Router();
+router.get('/all', UserController.findAllUsers);
+router.get('/:id', UserController.findById);
+router.post('/add', UserController.addUser);
+router.put('/updateUserLocation', UserController.updateUserLocation);
+module.exports = router;
